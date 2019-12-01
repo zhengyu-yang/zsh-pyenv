8e6f9811cf5862a4e7fc9e89b1fe35a96852052c
elony314 <elony314@gmail.com>
2019-11-30 16:23:02 -0800

    Update zsh-pyenv.plugin.zsh

--- zsh-pyenv.plugin.zsh
+++ zsh-pyenv.plugin.zsh
@@ -1,7 +1,25 @@
-# export PATH
+# export PYENV_HOME if it is not defined
 [[ -z "$PYENV_HOME" ]] && export PYENV_HOME="$HOME/.pyenv"
 
-export PATH="$PYENV_HOME/bin:$PATH"
+_zsh_pyenv_install() {
+    echo "Installing pyenv..."
+    curl https://pyenv.run | bash
+}
 
-eval "$(pyenv init - zsh)"
-eval "$(pyenv virtualenv-init - zsh)"
+
+_zsh_pyenv_load() {
+    export PATH="$PYENV_HOME/bin:$PATH"
+    eval "$(pyenv init - zsh)"
+    eval "$(pyenv virtualenv-init - zsh)"
+}
+
+# install pyenv if it isnt already installed
+[[ ! -d "$PYENV_HOME" ]] && _zsh_pyenv_install
+
+# load pyenv if it is installed
+if [[ -d "$PYENV_HOME" ]]; then
+    _zsh_pyenv_load
+fi
+
+unset -f _zsh_pyenv_install
+unset -f _zsh_pyenv_load
